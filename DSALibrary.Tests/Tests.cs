96142597470d09cf0f5d8df3fb78695642376426
Cody-Howell <codyhowell44@gmail.com>
2025-03-25 10:54:53 -0600

    Add project files.

---
+++ DSALibrary.Tests/Tests.cs
@@ -0,0 +1,88 @@
+﻿using DSALibrary.Dependency;
+using System.Threading.Tasks;
+using TUnit.Assertions.AssertConditions.Throws;
+
+namespace DSALibrary.Tests;
+
+public class Tests {
+    [Test]
+    public async Task CanCreateDependency() {
+        DependencyChecking<int> d = new DependencyChecking<int>([]);
+        await Assert.That(true).IsEqualTo(true);
+    }
+
+    [Test]
+    public async Task CanCreateSingleDependency() {
+        Dictionary<int, List<int>> dict = new Dictionary<int, List<int>> {
+            { 1, new List<int>() }
+        };
+        DependencyChecking<int> d = new DependencyChecking<int>(dict);
+
+        await Assert.That(d.Count).IsEqualTo(1);
+    }
+
+    [Test]
+    public async Task CanCreateTwoDependencies() {
+        Dictionary<int, List<int>> dict = new Dictionary<int, List<int>> {
+            { 1, new List<int>() },
+            { 2, new List<int>() { 1 } }
+        };
+
+        DependencyChecking<int> d = new DependencyChecking<int>(dict);
+
+        await Assert.That(d.Count).IsEqualTo(2);
+    }
+
+    [Test]
+    public async Task GeneratingDuplicateKeyThrowsException() {
+        await Assert.That(() =>
+        {
+            Dictionary<int, List<int>> dict = new Dictionary<int, List<int>> {
+                { 1, new List<int>() },
+                { 1, new List<int>() { 1 } }
+            };
+
+            DependencyChecking<int> d = new DependencyChecking<int>(dict);
+        })
+        .ThrowsExactly<ArgumentException>();
+    }
+
+    [Test]
+    public async Task CanGetMaximumDepth1() {
+        Dictionary<int, List<int>> dict = new Dictionary<int, List<int>> {
+            { 1, new List<int>() },
+            { 2, new List<int>() { 1 } }
+        };
+
+        DependencyChecking<int> d = new DependencyChecking<int>(dict);
+
+        await Assert.That(d.MaximumDepth).IsEqualTo(1);
+    }
+
+
+    [Test]
+    public async Task CanGetMaximumDepth2() {
+        Dictionary<int, List<int>> dict = new Dictionary<int, List<int>> {
+            { 1, new List<int>() },
+            { 2, new List<int>() { 1 } },
+            { 3, new List<int>() { 2 } }
+        };
+
+        DependencyChecking<int> d = new DependencyChecking<int>(dict);
+
+        await Assert.That(d.MaximumDepth).IsEqualTo(2);
+    }
+
+    [Test]
+    public async Task CanGetMaximumDepth3() {
+        Dictionary<int, List<int>> dict = new Dictionary<int, List<int>> {
+            { 1, new List<int>() },
+            { 2, new List<int>() { 1 } },
+            { 3, new List<int>() { 1 } }
+        };
+
+        DependencyChecking<int> d = new DependencyChecking<int>(dict);
+
+        await Assert.That(d.MaximumDepth).IsEqualTo(1);
+    }
+}
---
+++ DSALibrary/Dependency/DependencyChecking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DSALibrary.Dependency;
+
+public class DependencyChecking<T> where T : IEquatable<T> {
+    private readonly Dictionary<T, List<T>> values;
+
+    public DependencyChecking(Dictionary<T, List<T>> values) {
+        this.values = values;
+    }
+
+    public int Count { get => values.Count; }
+    public int MaximumDepth {
+        get {
+            List<T> distinct = new();
+            foreach (KeyValuePair<T, List<T>> pair in values) {
+                foreach (T value in pair.Value) {
+                    if (!distinct.Contains(value)) {
+                        distinct.Add(value);
+                    }
+                }
+            }
+            return distinct.Count;
+        }
+    }
+}
