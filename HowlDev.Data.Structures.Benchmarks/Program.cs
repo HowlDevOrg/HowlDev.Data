49e5e0368faf52f2e674d11ab6d7705aa0597145
Cody Howell <codyhowell44@gmail.com>
2026-05-15 18:02:32 -0600

    Benchmarks

---
+++ HowlDev.Data.Structures.Benchmarks/Program.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using HowlDev.Data.Structures.Games;
+
+namespace HowlDev.Data.Structures.Benchmarks;
+
+[ShortRunJob]
+public class ChessboardGetValueAtSquare {
+    private Chessboard board = new Chessboard();
+
+    [Benchmark]
+    public (ChessPiece? Piece, bool Color) GetSquare() => board.CheckSquare(0);
+}
+
+public class Program {
+    public static void Main(string[] args) {
+        BenchmarkRunner.Run<ChessboardGetValueAtSquare>();
+    }
+}
