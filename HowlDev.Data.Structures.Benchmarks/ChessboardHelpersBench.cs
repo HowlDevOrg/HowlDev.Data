12fc1026244239d89f36fc91fb8f9534a3c8ce08
Cody Howell <codyhowell44@gmail.com>
2026-05-21 15:37:05 -0600

    Added throwing benchmark
    Wow! That's way slower than I thought it would be, and takes up more memory too.

--- HowlDev.Data.Structures.Benchmarks/ChessboardHelpersBench.cs
+++ HowlDev.Data.Structures.Benchmarks/ChessboardHelpersBench.cs
@@ -10,5 +10,13 @@ public class ChessboardHelpersBench {
     [Benchmark]
     public int GetIndex() => ChessHelpers.RowColToIndex(1, 1);
     [Benchmark]
+    public int GetIndexThrowError() {
+        try {
+            return ChessHelpers.RowColToIndex(0, 9);
+        } catch {
+            return 0;
+        }
+    }
+    [Benchmark]
     public (int row, int col) GetRowCol() => ChessHelpers.IndexToRowCol(45);
 }
--- HowlDev.Data.Structures.Benchmarks/Program.cs
+++ HowlDev.Data.Structures.Benchmarks/Program.cs
@@ -4,13 +4,14 @@ namespace HowlDev.Data.Structures.Benchmarks;
 
 public class Program {
     public static void Main(string[] args) {
-        BenchmarkValidator.For<ChessboardGetValueAtSquareBench>()
-            .Expect("GetSquareReadonlyInlineStart", BenchmarkExpectations.ExpectedBytes(0).WithNanoseconds(0.9).WithMarginOfError(1.25))
-            .Expect("GetSquareReadonlyInlineEnd", BenchmarkExpectations.ExpectedBytes(0).WithNanoseconds(0.9).WithMarginOfError(1.25))
-            .Run();
+        // BenchmarkValidator.For<ChessboardGetValueAtSquareBench>()
+        //     .Expect("GetSquareReadonlyInlineStart", BenchmarkExpectations.ExpectedBytes(0).WithNanoseconds(0.9).WithMarginOfError(1.25))
+        //     .Expect("GetSquareReadonlyInlineEnd", BenchmarkExpectations.ExpectedBytes(0).WithNanoseconds(0.9).WithMarginOfError(1.25))
+        //     .Run();
 
         BenchmarkValidator.For<ChessboardHelpersBench>()
             .Expect("GetIndex", BenchmarkExpectations.ExpectedNanosecondsLessThan(0.1).WithBytes(0))
+            .Expect("GetIndexThrowError", BenchmarkExpectations.ExpectedMicroseconds(1.5).WithMarginOfError(1.25).WithBytes(320)) // :O
             .Expect("GetRowCol", BenchmarkExpectations.ExpectedNanosecondsLessThan(0.1).WithBytes(0))
             .Run();
     }
