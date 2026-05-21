5af213b17e8491d1df104a6b863ea51f37b1ef2b
Cody Howell <codyhowell44@gmail.com>
2026-05-21 17:24:01 -0600

    reorder

--- HowlDev.Data.Structures.Benchmarks/Program.cs
+++ HowlDev.Data.Structures.Benchmarks/Program.cs
@@ -10,14 +10,14 @@ public class Program {
             .Expect("GetRowCol", BenchmarkExpectations.ExpectedNanosecondsLessThan(0.1).WithBytes(0))
             .Run();
 
+        BenchmarkValidator.For<ChessboardInitBench>()
+            .Expect("NewBoard", BenchmarkExpectations.ExpectedNanoseconds(550).WithBytes(848))
+            .Run();
+
         BenchmarkValidator.For<ChessboardGetValueAtSquareBench>()
             .Expect("GetSquareReadonlyInlineStart", BenchmarkExpectations.ExpectedBytes(0).WithNanosecondsLessThan(1))
             .Expect("GetSquareReadonlyInlineReturnsNull", BenchmarkExpectations.ExpectedBytes(0).WithNanosecondsLessThan(1))
             .Expect("GetSquareReadonlyInlineEnd", BenchmarkExpectations.ExpectedBytes(0).WithNanosecondsLessThan(1))
-            .Run();
-
-        BenchmarkValidator.For<ChessboardInitBench>()
-            .Expect("NewBoard", BenchmarkExpectations.ExpectedNanoseconds(550).WithBytes(848))
             .Run();
     }
 }
