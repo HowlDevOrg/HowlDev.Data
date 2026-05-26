97afeaf8af670bceda2866cea1b3b309c52e41b3
Cody Howell <codyhowell44@gmail.com>
2026-05-25 19:52:08 -0600

    Updated runner for easier commenting-out.

--- HowlDev.Data.Structures.Benchmarks/Program.cs
+++ HowlDev.Data.Structures.Benchmarks/Program.cs
@@ -4,37 +4,42 @@ namespace HowlDev.Data.Structures.Benchmarks;
 
 public class Program {
     public static void Main(string[] _) {
-        BenchmarkValidator.For<ChessboardHelpersBench>()
+        HelpersBench.Run();
+        ValueAtSquareBench.Run();
+        ValidMovesBench.Run();
+        ChessMoveBench.Run();
+    }
+
+    private static BenchmarkValidatorWithConfig<ChessMoveBench> ChessMoveBench =>
+        BenchmarkValidator.For<ChessMoveBench>()
+            .WithProfile(BenchmarkProfiles.ShortRun)
+            .WithMemoryDiagnoser()
+            .WithoutLogOutput()
+            .Expect("DefaultMove", BenchmarkExpectations.ExpectedNanoseconds(175).WithBytes(904));
+
+    private static BenchmarkValidatorWithConfig<ChessboardPieceGetValidMovesBench> ValidMovesBench =>
+        BenchmarkValidator.For<ChessboardPieceGetValidMovesBench>()
             .WithProfile(BenchmarkProfiles.SilentShortRun)
             .WithMemoryDiagnoser()
-            .Expect("GetIndex", BenchmarkExpectations.ExpectedNanosecondsLessThan(0.1).WithBytes(0))
-            .Expect("GetRowCol", BenchmarkExpectations.ExpectedNanosecondsLessThan(0.1).WithBytes(0))
-            .Run();
+            .Expect("GetKingMoves", BenchmarkExpectations.ExpectedNanoseconds(35).WithBytes(144))
+            .Expect("GetKnightMoves", BenchmarkExpectations.ExpectedNanoseconds(38).WithBytes(144))
+            .Expect("GetBishopMoves", BenchmarkExpectations.ExpectedNanoseconds(52).WithBytes(200))
+            .Expect("GetRookMoves", BenchmarkExpectations.ExpectedNanoseconds(95).WithBytes(240))
+            .Expect("GetQueenMoves", BenchmarkExpectations.ExpectedNanoseconds(130).WithBytes(312))
+            .Expect("GetPawnMoves", BenchmarkExpectations.ExpectedNanoseconds(30).WithBytes(104));
 
+    private static BenchmarkValidatorWithConfig<ChessboardGetValueAtSquareBench> ValueAtSquareBench =>
         BenchmarkValidator.For<ChessboardGetValueAtSquareBench>()
             .WithProfile(BenchmarkProfiles.SilentShortRun)
             .WithMemoryDiagnoser()
             .Expect("GetSquareReadonlyInlineStart", BenchmarkExpectations.ExpectedBytes(0).WithNanosecondsLessThan(1))
             .Expect("GetSquareReadonlyInlineReturnsNull", BenchmarkExpectations.ExpectedBytes(0).WithNanosecondsLessThan(1))
-            .Expect("GetSquareReadonlyInlineEnd", BenchmarkExpectations.ExpectedBytes(0).WithNanosecondsLessThan(1))
-            .Run();
+            .Expect("GetSquareReadonlyInlineEnd", BenchmarkExpectations.ExpectedBytes(0).WithNanosecondsLessThan(1));
 
-        BenchmarkValidator.For<ChessboardPieceGetValidMovesBench>()
+    private static BenchmarkValidatorWithConfig<ChessboardHelpersBench> HelpersBench =>
+        BenchmarkValidator.For<ChessboardHelpersBench>()
             .WithProfile(BenchmarkProfiles.SilentShortRun)
             .WithMemoryDiagnoser()
-            .Expect("GetKingMoves", BenchmarkExpectations.ExpectedNanoseconds(35).WithBytes(144))
-            .Expect("GetKnightMoves", BenchmarkExpectations.ExpectedNanoseconds(35).WithBytes(144))
-            .Expect("GetBishopMoves", BenchmarkExpectations.ExpectedNanoseconds(52).WithBytes(200))
-            .Expect("GetRookMoves", BenchmarkExpectations.ExpectedNanoseconds(90).WithBytes(240))
-            .Expect("GetQueenMoves", BenchmarkExpectations.ExpectedNanoseconds(130).WithBytes(312))
-            .Expect("GetPawnMoves", BenchmarkExpectations.ExpectedNanoseconds(25).WithBytes(104))
-            .Run();
-
-        BenchmarkValidator.For<ChessMoveBench>()
-            .WithProfile(BenchmarkProfiles.ShortRun)
-            .WithMemoryDiagnoser()
-            .WithoutLogOutput()
-            .Expect("DefaultMove", BenchmarkExpectations.ExpectedNanoseconds(175).WithBytes(928))
-            .Run();
-    }
+            .Expect("GetIndex", BenchmarkExpectations.ExpectedNanosecondsLessThan(0.1).WithBytes(0))
+            .Expect("GetRowCol", BenchmarkExpectations.ExpectedNanosecondsLessThan(0.1).WithBytes(0));
 }
